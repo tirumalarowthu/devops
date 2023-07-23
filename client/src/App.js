2cbf3911a8ab5fa0c0773e341e817efa4bdf0769
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-07-23 11:20:02 +0530

    ok

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello </h2>
+      <h4>This is my first project ,Jai sriram</h4>
+    </div>
+  );
+}
+
+export default App;
